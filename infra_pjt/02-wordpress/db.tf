3ef504ca119b9342dcdb044cd86256210b9ca359
conti0513 <yoshimasa@sawadesign.jp>
2023-04-04 08:26:19 +0900

    infra

--- infra_pjt/02-wordpress/db.tf
+++ infra_pjt/02-wordpress/db.tf
@@ -18,6 +18,7 @@ resource "aws_db_instance" "wordpress_rds" {
   username             = "wordpress_admin"
   password             = "testpassword"
   parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
   db_subnet_group_name = aws_db_subnet_group.wordpress_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 }
--- tutorial_docker/README.md
+++ tutorial_docker/README.md
@@ -6,6 +6,8 @@ tutorial_docker
 ├── docker-php      # DIR
 ├── docker-php2     # DIR
 ├── docker-php-only # DIR PHP & Apache
-├── readme.txt      # This file. About this DIR.
+├── README.md       # This file. About this DIR.
+
+--- docker-terraform
 
 
