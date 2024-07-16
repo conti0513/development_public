890c5f7564da0c5886fe5ce351852a92765e3ae3
yoshimasa kondo <124630091+conti0513@users.noreply.github.com>
2024-07-17 00:17:41 +0900

    modify create_workflow.sh

--- infra_pjt/scripts/create_workflow.sh
+++ infra_pjt/scripts/create_workflow.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo "Do you want to create a normal workflow or a rollback workflow? (Enter 'normal' or 'rollback'):"
-read workflow_type
-
-echo "Enter the project name for which you want to create the workflow (e.g., project1, project2):"
-read project_name
+read -p "Enter workflow type (normal/rollback): " workflow_type
+read -p "Enter project name: " project_name
 
 if [ "$workflow_type" == "normal" ]; then
-  cat <<EOL > /workspaces/development_public/.github/workflows/docker-publish-${project_name}.yml
-name: Build and Push Docker Image for ${project_name}
+  cat <<EOL > /workspaces/development_public/.github/workflows/docker-publish.yml
+name: Build and Push Docker Image
 
 on:
   push:
@@ -16,7 +13,7 @@ on:
       - main
 
 jobs:
-  build-and-push-${project_name}:
+  build-and-push-python:
     runs-on: ubuntu-latest
 
     steps:
@@ -35,25 +32,22 @@ jobs:
       - name: Build and push
         uses: docker/build-push-action@v2
         with:
-          context: ./infra_pjt/python_projects/${project_name}
-          file: ./infra_pjt/common/Dockerfile
+          context: ./infra_pjt/common
           push: true
-          tags: butainco/${project_name}:latest
+          tags: butainco/\${project_name}:latest
+
 EOL
-
-  echo "Normal workflow for ${project_name} created successfully."
-
 elif [ "$workflow_type" == "rollback" ]; then
-  cat <<EOL > /workspaces/development_public/.github/workflows/docker-rollback-${project_name}.yml
-name: Rollback Docker Image for ${project_name}
+  cat <<EOL > /workspaces/development_public/.github/workflows/docker-publish.yml
+name: Build and Push Docker Image
 
 on:
   push:
     branches:
-      - rollback
+      - main
 
 jobs:
-  rollback-${project_name}:
+  build-and-push-python:
     runs-on: ubuntu-latest
 
     steps:
@@ -69,17 +63,14 @@ jobs:
           username: \${{ secrets.DOCKER_USERNAME }}
           password: \${{ secrets.DOCKER_PASSWORD }}
 
-      - name: Rollback
+      - name: Build and push
         uses: docker/build-push-action@v2
         with:
-          context: ./infra_pjt/python_projects/${project_name}
-          file: ./infra_pjt/common/Dockerfile
+          context: ./infra_pjt/common
           push: true
-          tags: butainco/${project_name}:rollback
+          tags: butainco/\${project_name}:rollback
+
 EOL
-
-  echo "Rollback workflow for ${project_name} created successfully."
-
 else
   echo "Invalid workflow type. Please enter 'normal' or 'rollback'."
 fi
