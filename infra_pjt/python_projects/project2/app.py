6d57ece3598b4b709f2d1a42475fa4c9dc5fbeeb
yoshimasa kondo <124630091+conti0513@users.noreply.github.com>
2024-07-17 06:57:41 +0900

    modified P1

--- infra_pjt/python_projects/project2/app.py
+++ infra_pjt/python_projects/project2/app.py
@@ -1,14 +1,2 @@
-import csv
-import webbrowser
+print(Hello Python_Project2!)
 
-def open_urls_from_csv(csv_file):
-    with open(csv_file, newline='') as csvfile:
-        url_reader = csv.reader(csvfile)
-        for row in url_reader:
-            if row:  # check if row is not empty
-                url = row[0].strip()  # assuming URL is in the first column
-                webbrowser.open(url)
-
-if __name__ == "__main__":
-    csv_file_path = "/workspaces/development_public/infra_pjt/python_projects/project2/urls.csv"
-    open_urls_from_csv(csv_file_path)
