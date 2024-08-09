1e75a681bb71c8a7384c7f9dd3f706009d7fe6b7
yoshimasa kondo <124630091+conti0513@users.noreply.github.com>
2024-08-10 07:48:06 +0900

    Initial commit before removing credentials

--- projects/aws_projects/experience2_flexible_monitoring/scripts/deploy.sh
+++ projects/aws_projects/experience2_flexible_monitoring/scripts/deploy.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# 開発環境向けデプロイメント手順
+# Deployment procedure for development environment
 echo "Deploying to development"
 
-# EC2インスタンスを起動
+# Launch EC2 instance
 INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0c55b159cbfafe1f0 --count 1 --instance-type t2.micro --key-name MyKeyPair --query 'Instances[0].InstanceId' --output text)
 echo "Instance ID: $INSTANCE_ID"
 
-# インスタンスが実行状態になるまで待機
+# Wait until the instance is running
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 echo "Instance $INSTANCE_ID is running"
 
-# インスタンスのパブリックIPアドレスを取得
+# Get the public IP address of the instance
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
 echo "Public IP: $PUBLIC_IP"
 
-# SSHを使用して "Hello World" を出力
-ssh -o StrictHostKeyChecking=no -i /path/to/MyKeyPair.pem ec2-user@$PUBLIC_IP "echo 'Hello World'"
+# Use SSH to output "Hello World"
+ssh -o StrictHostKeyChecking=no -i /workspaces/development_public/projects/aws_projects/sawadesign_test01.pem ec2-user@$PUBLIC_IP "echo 'Hello World'"
 
-# デプロイメントが完了したらインスタンスを終了
+# Terminate the instance after deployment is complete
 aws ec2 terminate-instances --instance-ids $INSTANCE_ID
 aws ec2 wait instance-terminated --instance-ids $INSTANCE_ID
 echo "Instance $INSTANCE_ID terminated"
-
