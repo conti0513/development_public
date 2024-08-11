bf80afaad5878491b56ca6e281f1bd021460f38b
yoshimasa kondo <124630091+conti0513@users.noreply.github.com>
2024-08-12 04:36:00 +0900

    Configured Terraform workflow for EC2 instance deployment with security group

---
+++ fetch_public_ip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get the public IP address of the current machine.
+PUBLIC_IP=$(curl -s http://whatismyip.akamai.com/)
+
+# Save the IP address to a Terraform variable file.
+echo "public_ip = \"${PUBLIC_IP}/32\"" > terraform/vars.tf
+
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,63 @@
+# Provider Configuration: Sets the AWS region to be used for deploying resources.
 provider "aws" {
-  region = var.aws_region
+  region = var.aws_region  # Uses the AWS region defined in the variables below
 }
 
-# 全てのIPからのSSHアクセスを許可するセキュリティグループを作成
+# Security Group: Creates a security group allowing SSH access from any IP and permits all outbound traffic.
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh_from_anywhere"
-  description = "Allow SSH inbound traffic from anywhere"
-  vpc_id      = var.vpc_id
+  name        = "allow_ssh_from_anywhere"  # Name of the security group
+  description = "Allow SSH inbound traffic from anywhere"  # Description of the security group
+  vpc_id      = var.vpc_id  # The VPC ID where this security group will be created
 
+  # Inbound rule: Allow SSH access on port 22 from any IP address
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # すべてのIPアドレスを許可
+    from_port   = 22  # Start of port range (22 for SSH)
+    to_port     = 22  # End of port range
+    protocol    = "tcp"  # Protocol used, TCP for SSH
+    cidr_blocks = ["0.0.0.0/0"]  # Allow from any IP address
   }
 
+  # Outbound rule: Allow all outbound traffic
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"  # すべてのアウトバウンドトラフィックを許可
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0  # Start of port range
+    to_port     = 0  # End of port range
+    protocol    = "-1"  # Protocol -1 allows all traffic
+    cidr_blocks = ["0.0.0.0/0"]  # Allow to any IP address
   }
 
   tags = {
-    Name = "allow_ssh_from_anywhere"
+    Name = "allow_ssh_from_anywhere"  # Tag for the security group
   }
 }
 
-# EC2インスタンスの起動
+# EC2 Instance: Launches a t2.micro EC2 instance in the Tokyo region, attaching the security group for SSH access.
 resource "aws_instance" "example" {
-  ami           = "ami-0c08c226b5ff7874c" # TokyoリージョンのAmazon Linux 2 AMI ID
-  instance_type = "t2.micro"
-  key_name      = var.key_name
-  security_groups = [aws_security_group.allow_ssh.name]  # セキュリティグループをアタッチ
+  ami           = "ami-0c08c226b5ff7874c" # Amazon Linux 2 AMI ID for Tokyo region
+  instance_type = "t2.micro"  # Instance type (small and low cost)
+  key_name      = var.key_name  # SSH key pair name, provided as a variable
+  security_groups = [aws_security_group.allow_ssh.name]  # Attach the created security group
 
   tags = {
-    Name = "TerraformExample"
+    Name = "TerraformExample"  # Tag for the EC2 instance
   }
 }
 
+# Variables: Used to configure the AWS region, SSH key pair, and VPC ID dynamically, making the configuration more flexible.
+
+# AWS region
 variable "aws_region" {
   description = "The AWS region"
   type        = string
-  default     = "ap-northeast-1"  # Tokyoリージョンをデフォルトに設定
+  default     = "ap-northeast-1"  # Default to Tokyo region
 }
 
+# SSH key pair name
 variable "key_name" {
   description = "The name of the SSH key pair"
   type        = string
 }
 
+# VPC ID
 variable "vpc_id" {
   description = "The VPC ID where the security group will be created"
   type        = string
