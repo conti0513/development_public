4152a01806f761be57fdccbaf353fc22cce7023e
yoshimasa kondo <124630091+conti0513@users.noreply.github.com>
2024-08-11 17:59:52 +0900

    modified main.tf_SG_Setting

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,37 @@ provider "aws" {
   region = var.aws_region
 }
 
+# Create a security group that allows SSH access
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Allows all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+# Launch an EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-0c08c226b5ff7874c" # AMI ID for Amazon Linux 2 in Tokyo
   instance_type = "t2.micro"
   key_name      = var.key_name
+  security_groups = [aws_security_group.allow_ssh.name]  # Attach the security group
 
   tags = {
     Name = "TerraformExample"
@@ -20,5 +47,10 @@ variable "aws_region" {
 
 variable "key_name" {
   description = "The name of the SSH key pair"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The VPC ID where the security group will be created"
   type        = string
 }
